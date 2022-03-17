0d5c57b7da7a711d3bf2ee924a95b5ac6a49a62f
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2022-03-17 11:26:10 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,38 +1,47 @@
+// Endre meg! https://rapidapi.com/api-sports/api/covid-193/
+const apikey = "EndreMeg!"
+
 const express = require('express')
-const axios = require("axios").default;
 const pug = require('pug')
 const app = express()
 const port = 3000
 
+
+const axios = require("axios").default;
+var options = {
+  method: 'GET',
+  url: 'https://covid-193.p.rapidapi.com/statistics',
+  params: {country: "Norway"},
+  headers: {
+    'x-rapidapi-host': 'covid-193.p.rapidapi.com/statistics',
+    'x-rapidapi-key': apikey
+  }
+};
+
+
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
-  var axios = require("axios").default;
-
-  var options = {
-    method: 'GET',
-    url: 'https://covid-193.p.rapidapi.com/statistics',
-    params: {country: 'Norway'},
-    headers: {
-      'x-rapidapi-host': 'covid-193.p.rapidapi.com',
-      'x-rapidapi-key': 'HentDinEgen https://rapidapi.com/api-sports/api/covid-193/'
-    }
-  };
+  var country = req.query.country;
+  country ??= "Norway"; // Default value
+  options.params.country = country;
 
   axios.request(options).then(function (response) {
-    // Se https://rapidapi.com/api-sports/api/covid-193/ for hva du kan hente,
-    // ['response'][0]['... mye rart her!']
     var data = response.data['response'][0]['cases']
+
     var newCases = data['new']
-    var activeCases = data['active']
-    res.render('index', { newCases: newCases, activeCases: activeCases})
+    newCases ??= "0!! :)";
+    var activeCases = data['total']
+
+    res.render('index', {country: country, newCases: newCases, activeCases: activeCases})
 
   }).catch(function (error) {
-  	console.error(error);
+    // .catch() kjører ved feilmelding
+    res.send(error)
   });
 })
 
 
 app.listen(port, () => {
-  console.log(`Se nettsiden i nettleser; http://localhost:${port}`)
+  console.log(`Se nettsiden i nettleser; localhost:${port}`)
 })
